e7e20d2dc1fbe1113a58e178a9cd5811b9d8c5a6
Jim Cooper <jim@cooperplanet.com>
2022-06-14 09:20:14 -0600

    Add head title and description.

--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -1,11 +1,19 @@
+import Head from 'next/head'
+
 import { CartProvider } from '../../context-providers/cart-provider'
 import SiteHeader from '../site-header/SiteHeader'
 
 export default function Layout(props) {
   return (
-    <CartProvider>
-      <SiteHeader user={props.user} />
-      {props.children}
-    </CartProvider>
+    <>
+      <Head>
+        <title>Joe&apos;s Robot Shop</title>
+        <meta name="description" content="Buy robot parts and build awesome robots!" />
+      </Head>
+      <CartProvider>
+        <SiteHeader user={props.user} />
+        {props.children}
+      </CartProvider>
+    </>
   )
 }
