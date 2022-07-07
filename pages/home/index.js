1fecd015a6e94eecded1a8f62d1efdb3f1d44af9
Jim Cooper <jim@cooperplanet.com>
2022-07-06 23:27:04 -0600

    Stub out album home page

--- pages/home/index.js
+++ pages/home/index.js
@@ -3,89 +3,61 @@ import Link from 'next/link'
 
 import styles from './Home.module.scss'
 import {
-  headFriendly,
-  headBigEye,
-  headShredder,
-  armArticulatedClaw,
-  torsoGauged,
-  baseSpring,
-} from '../../public/images/robot-images'
+  whyWeSuffer,
+  dearMisery,
+} from '../../public/images/albums'
 
-import robotApocalypse from './robot-apocalypse.png'
+import inspiration from './inspiration.jpg'
 
 function Home() {
   return (
     <div className={styles.container}>
       <div className={styles.hero}></div>
 
-      <div className={styles.promoted}>
-        <span className={styles.image}>
-          <Image src={headFriendly} alt='Friendly Robot Head' />
-        </span>
-        <div className={styles.promoText}>
-          <div className={styles.promoMainText}>
-            DISPELL THE ROBOT APOCALYPSE MYTH
-          </div>
-          <div className={`${styles.promoSubText} cta`}>
-            <div>SAVE 20% ON OUR FRIENDLIEST</div>
-            <div>ROBOT HEADS</div>
-          </div>
+      <div className={styles.cta}>
+        <div className={styles.images}>
+          <span className={styles.image}>
+            <Image src={whyWeSuffer} alt='Why We Suffer Album' />
+          </span>
+          <span className={styles.image}>
+            <Image src={dearMisery} alt='Dear Misery Album' />
+          </span>
         </div>
-        <span className={styles.image}>
-          <Image src={headBigEye} alt='Big Eye Head' />
-        </span>
+        <div className={styles.ctaText}>
+          <div className={styles.ctaMainText}>
+            Your <em>Album</em> and <em>Artist</em> Name
+          </div>
+          <div className={`${styles.ctaSubText}`}>
+            <div>on <em>Custom</em> Album Designs</div>
+          </div>
+          <button className="buton cta">Shop Now</button>
+        </div>
+        <div className={styles.images}>
+          <span className={styles.image}>
+            <Image src={dearMisery} alt='Dear Misery Album' />
+          </span>
+          <span className={styles.image}>
+            <Image src={whyWeSuffer} alt='Why We Suffer Album' />
+          </span>
+        </div>
       </div>
 
-      <ul className={styles.robotPartsCta}>
-        <li className={styles.part}>
-          <Link href={{ pathname: '/catalog', query: { filter: 'Heads' } }}>
-            <a>
-              <div className={styles.image}><Image src={headShredder} alt='Robot Heads' width={180} height={180} /></div>
-              <div>ROBOT HEADS</div>
-            </a>
-          </Link>
-        </li>
-        <li className={styles.part}>
-          <Link href={{ pathname: '/catalog', query: { filter: 'Arms' } }}>
-            <a>
-              <div className={styles.image}><Image src={armArticulatedClaw} alt='Robot Arms' width={180} height={180} /></div>
-              <div>ROBOT ARMS</div>
-            </a>
-          </Link>
-        </li>
-        <li className={styles.part}>
-          <Link href={{ pathname: '/catalog', query: { filter: 'Torsos' } }}>
-            <a>
-              <div className={styles.image}><Image src={torsoGauged} alt='Robot Torsos' width={180} height={180} /></div>
-              <div>ROBOT TORSOS</div>
-            </a>
-          </Link>
-        </li>
-        <li className={styles.part}>
-          <Link href={{ pathname: '/catalog', query: { filter: 'Bases' } }}>
-            <a>
-              <div className={styles.image}><Image src={baseSpring} alt='Robot Bases' width={180} height={180} /></div>
-              <div>ROBOT BASES</div>
-            </a>
-          </Link>
-        </li>
-      </ul>
 
       <div className={styles.whitePaper}>
         <div className={styles.image}>
-          <Image src={robotApocalypse} alt='Robot Apocalyse' />
+          <Image layout="fill" src={inspiration} alt='Robot Apocalyse' />
         </div>
         <div className={styles.text}>
           <div>
             <div className={`${styles.headerText} cta`}>
-              Will they kill us all?
+              Stuck?
             </div>
             <div className={styles.subText}>
-              <p>10 Myths About the</p>
-              <p>Robot Apocalyse</p>
+              <p>10 Steps to Shatter</p>
+              <p>Writer's Block</p>
             </div>
           </div>
-          <div className={styles.largeText}>WHITE PAPER</div>
+          <div className={styles.largeText}>WORKSHOP</div>
           <Link href='/'>
             <a className={styles.learnMore}>Learn More</a>
           </Link>
---
+++ public/images/albums/index.js
@@ -0,0 +1,2 @@
+export { default as whyWeSuffer } from './why-we-suffer.png'
+export { default as dearMisery } from './dear-misery.jpg'
